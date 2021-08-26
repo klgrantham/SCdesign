2010a4dab5bd38dc679844a2ab08a24edb849827
Kelsey L Grantham <kelsey.grantham@monash.edu>
2021-08-26 14:09:54 +1000

    Include comparisons assuming linear period effects

--- comparisons.R
+++ comparisons.R
@@ -8,14 +8,17 @@ source('variances.R')
 # SC design with 3 clusters, 2 periods per sequence
 # Note: This is the embedded staircase design
 varSCSW_plot_general(10, 3, 1, 10, 3, 1, 1, 1, 0, 'cat')
+varSCSW_plot_general(10, 3, 1, 10, 3, 1, 1, 1, 0, 'lin')
 
 # SW design with 3 clusters, 4 periods vs.
 # SC design with 6 clusters, 2 periods per sequence
 # (6+1+1-1=7 periods total)
 # Note: Designs have same total number of cluster-periods
 varSCSW_plot_general(10, 3, 1, 10, 6, 1, 1, 1, 0, 'cat')
+varSCSW_plot_general(10, 3, 1, 10, 6, 1, 1, 1, 0, 'lin')
 # SC design with 6 clusters (3 sequences, each repeated twice), 2 periods each
 varSCSW_plot_general(10, 3, 1, 10, 3, 1, 1, 2, 0, 'cat')
+varSCSW_plot_general(10, 3, 1, 10, 3, 1, 1, 2, 0, 'lin')
 
 # SW design with 3 clusters, 4 periods vs.
 # SC design with 3 clusters, 4 periods per sequence
